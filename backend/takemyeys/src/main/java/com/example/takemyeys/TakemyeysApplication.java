5245300e0e8b179967da1a3a6d06957b85c94629
Drine Ihssene <drinodroid@gmail.com>
2018-08-28 22:58:29 +0200

    add backend

---
+++ backend/takemyeys/src/main/java/com/example/takemyeys/TakemyeysApplication.java
@@ -0,0 +1,12 @@
+package com.example.takemyeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakemyeysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakemyeysApplication.class, args);
+	}
+}
